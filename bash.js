18ac77ec15a190e14457e0e731e0b0b21dcb7531
Chris Zhen <chris.zhenartist@gmail.com>
2022-04-22 12:21:59 -0400

    process global

---
+++ bash.js
@@ -0,0 +1,11 @@
+process.stdout.write("prompt >");
+
+process.stdin.on("data", (data) => {
+  const cmd = data.toString().trim();
+
+  process.stdout.write("You typed: " + cmd);
+
+  if (cmd === "pwd") process.stdout.write("\n" + process.cwd());
+
+  process.stdout.write("\nprompt >");
+});
